a321836443c9bf415203b0bbde9a34d5e5944c29
peterortiz82 <peterortiz82@gmail.com>
2017-05-02 20:42:27 -0400

    updated devise form

--- db/migrate/20170503002420_add_first_name_and_last_name_to_user.rb
+++ db/migrate/20170503002420_add_first_name_and_last_name_to_user.rb
@@ -3,8 +3,7 @@
 class AddFirstNameAndLastNameToUser < ActiveRecord::Migration[5.0]
 
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
+    add_column :users, :name, :string
   end
 
 end
