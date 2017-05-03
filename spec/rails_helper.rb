35c3331198b5abeb1b7b3dffa2e69a8a62d21ead
peterortiz82 <peterortiz82@gmail.com>
2017-05-02 20:11:14 -0400

    install testing gems and basic config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,18 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'factory_girl_rails'
+require 'database_cleaner'
+require 'capybara/rails'
+require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -29,6 +40,9 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # Prevents from having to call FactoryGirl.create, build, ect. within the spec.
+  config.include FactoryGirl::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
