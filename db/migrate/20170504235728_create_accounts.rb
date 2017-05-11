77a8e1f6936b1ca7164ecfea18eb92e8e6967af9
peterortiz82 <peterortiz82@gmail.com>
2017-05-10 20:16:09 -0400

    trying to get things right

--- db/migrate/20170504235728_create_accounts.rb
+++ db/migrate/20170504235728_create_accounts.rb
@@ -5,7 +5,7 @@ class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
       t.string :name
-      t.references :organizations, foreign_key: true
+      t.references :organization, foreign_key: true
 
       t.timestamps
     end
