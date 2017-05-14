ce224415c36c6cfc329dc4a4da8f602ed4eeedf8
peterortiz82 <peterortiz82@gmail.com>
2017-05-14 03:05:21 -0400

    another change

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 25 }
+  config.redis = { :size => 3 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 25 }
+  
 end
