1a0bc68db7bce378395b8a00330820dafca260a4
peterortiz82 <peterortiz82@gmail.com>
2017-05-13 22:59:11 -0400

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,13 @@
 class HomeController < ApplicationController
 
   def index
-    binding.pry
+    puts "#################################"
+    puts "#################################"
+    puts current_user.present?
+    puts current_user.name
+    puts current_user.sign_in_count
+    puts "#################################"
+    puts "#################################"
     if current_user.present?
       redirect_to  organization_accounts_path
     end
