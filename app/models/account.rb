f011e4d22928c3a3a660954b7cacbe9039be0975
peterortiz82 <peterortiz82@gmail.com>
2017-05-13 22:41:02 -0400

    fixed typo

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@
 class Account < ApplicationRecord
 
   belongs_to :organization
-  has_many :connection_accountsm, dependent: :destroy
+  has_many :connection_accounts, dependent: :destroy
 
   validates :name, presence: true
   validates :organization_id, presence: true
